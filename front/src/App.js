b093e190583dde469a1e071694a59d3deef04f3c
George <42513185+RympeR@users.noreply.github.com>
2022-10-26 20:58:14 +0300

    minor css update

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
 import { LangProvider } from "./LangProvider";
 
 import { AboutBlock } from "./components/ContentBlocks/AboutBlock";
