d3d5d639d6212c981aa0f984b2064f912363a1ea
George Rashkov <georg.rashkov@gmail.com>
2022-11-13 12:19:51 +0200

    added menu img

--- core/landing/admin.py
+++ core/landing/admin.py
@@ -3,7 +3,7 @@ from .models import Contact
 
 @admin.register(Contact)
 class ContactAmin(admin.ModelAdmin):
-    list_display = ('email', 'short_text', 'created_pretty')
+    list_display = ('email', 'text', 'created_pretty')
     list_filter = ('created',)
     search_fields = ('email',)
     ordering = ('-created',)
